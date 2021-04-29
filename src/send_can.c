568939876173e976c07b0de4f7e3b5639007218a
heisenbl <heisenbl@hs-mittweida.de>
2021-04-29 11:14:26 +0200

    First prototype of can sender

--- src/send_can.c
+++ src/send_can.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
 
 #include "can/message.h"
 
@@ -7,19 +16,51 @@ int main(void)
 {
     message_t message = 
     {
-        .priority = 10,
+        .priority = 2,
         .sleep_in_ms = 2000,
-        .text = "abcdefghij"
+        .text = "abcdefghi"
     };
 
-    serialized_message_t serialized_message = 
+    int s; 
+    struct sockaddr_can addr;
+    struct ifreq ifr;
+    struct can_frame frame;
+
+    /* create CAN socket */
+    if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
     {
-        .message = message
-    };
+        perror("Socket");
+        return 1;
+    }
 
-    char* command_base = "cansend can0 1#";
-    char* command = strcat(command_base, serialized_message.buffer); 
-    system(command);
+    /* receive interface index */
+    strcpy(ifr.ifr_name, "can0");
+	ioctl(s, SIOCGIFINDEX, &ifr);
 
-    return EXIT_SUCCESS;
+    /* bind CAN socket */
+	memset(&addr, 0, sizeof(addr));
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("Bind");
+		return 1;
+	}
+
+    /* send CAN frame */
+	frame.can_id = 0x1;
+	frame.can_dlc = 5;
+	sprintf(frame.data, "Hello");
+
+	if (write(s, &frame, sizeof(struct can_frame)) != sizeof(struct can_frame)) {
+		perror("Write");
+		return 1;
+	}
+
+	if (close(s) < 0) {
+		perror("Close");
+		return 1;
+	}
+
+    return 0;
 }
