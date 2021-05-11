428f8aaa0ce3d8c09d12a043da7a001a0e0587cc
Jan Eilers <jeilers@hs-mittweida.de>
2021-05-11 11:17:51 +0200

    variable device path and better error messages

--- src/send_uart.c
+++ src/send_uart.c
@@ -6,12 +6,24 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <linux/limits.h>
 
-int main(void){
+int main(int argc, char* argv[]){
+    char path[PATH_MAX];
+    if(argc < 2){
+        printf("USAGE: %s DEVICEPATH\n", argv[0]);
+        printf("No device specified, defaulting to /dev/ttyACM1\n\n");
+        strcpy(path, "/dev/ttyACM1");
+    } else {
+        strcpy(path, argv[1]);
+    }
+
+
     int uart_fs = -1;
-    uart_fs = open("/dev/ttyACM1", O_RDWR | O_NOCTTY | O_NDELAY);
+    uart_fs = open(path, O_RDWR | O_NOCTTY | O_NDELAY);
     if(uart_fs == -1) {
-        printf("[ERROR] UART open\n");
+        printf("[ERROR] UART open:\n"); 
+        printf("%s\n", strerror(errno));
         return EXIT_FAILURE;
     }
 
@@ -24,17 +36,16 @@ int main(void){
     tcflush(uart_fs, TCIFLUSH);
     tcsetattr(uart_fs, TCSANOW, &options);
 
-    const struct Message msg = {0, 1000, "hello\0"};
+    const struct Message msg = {0, 1000, "hello_usb"};
     union Serialized_Message data;
     data.message = msg;
 
     int tx_len = write(uart_fs, data.buffer, sizeof(data.buffer));
     if(tx_len < 0) {
         printf("[ERROR] UART write\n");
+        printf("%s\n", strerror(errno));
         return EXIT_FAILURE;
     }
-
-    sleep(1);
 
     int rx_len;
     union Serialized_Message rx_data;
@@ -42,7 +53,7 @@ int main(void){
     rx_len = read(uart_fs, rx_data.buffer, sizeof(struct Message));
     if(rx_len < 0){
         printf("[ERROR] UART read\n");
-        printf("Error: %s\n", strerror(errno));
+        printf("%s\n", strerror(errno));
         return EXIT_FAILURE;
     } else if(rx_len == 0) {
         printf("[ERROR] No data\n");
